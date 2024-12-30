3069c0cc7b02220277e6a90bc308b73e5821840a
KARTHICK V <karthickssp00@gmail.com>
2024-12-30 06:40:22 +0530

    Update Username
    Username added to extension

--- popup.js
+++ popup.js
@@ -2,7 +2,9 @@ let searchLimit = 0;
 let searchCount = 0;
 let isPaused = false;
 let activeAutomation = null;
+let username = "Store the email id...";
 const today = new Date().getDate();
+const headerName = document.getElementById("header-span");
 const focusTabsCheckbox = document.getElementById("focusTabs");
 const searchCountDropdown = document.getElementById("searchCount");
 const customTimerInput = document.getElementById("customTimerInput");
@@ -50,6 +52,7 @@ function updateUI(searchCount, searchLimit) {
 }
 chrome.storage.sync.get(
   {
+    username: "Store the email id...",
     focusTabs: false,
     searchCount: 0,
     customTimer: 0,
@@ -68,11 +71,13 @@ chrome.storage.sync.get(
     timerDropdown.value = data.customTimer > 0 ? data.customTimer / 1000 : 0;
     customTimerInput.value = data.customTimer / 1000 || "";
     searchLimit = parseInt(data.searchCount, 10) || 0;
+    console.log("Username: ", data.username);
     console.log("Searched Query count: ", searchCount);
     console.log("Search limit: ", searchLimit);
     console.log("Focus tabs: ", data.focusTabs);
     console.log("Custom timer: ", data.customTimer);
     console.log("Last date: ", data.lastDate);
+    headerName.textContent = data.username;
     limitInfo.style.display = searchLimit > 0 ? "none" : "block";
     if (searchLimit === 0) {
       limitInfo.textContent =
@@ -89,24 +94,54 @@ saveButton.addEventListener("click", () => {
     alert("Please enter a valid search count.");
     return;
   }
-  chrome.storage.sync.set(
-    {
-      focusTabs: focusTabsCheckbox.checked,
-      searchCount: newSearchLimit,
-      customTimer: customTimer,
-      lastDate: today,
-      lastSearchCount: searchCount,
-    },
-    () => {
-      updateUI(0, newSearchLimit);
-      console.log("Searched Query count: ", searchCount);
-      console.log("Search limit: ", newSearchLimit);
-      console.log("Custom timer: ", customTimer);
-      console.log("Last date: ", today);
-      console.log("Focus tabs: ", focusTabs.checked);
-      alert("Settings Saved!!! Please refresh the page.");
+  if(username === "Store the email id..."){
+    username = prompt("Please enter your email id to proceed.");
+    if(username && username.includes("@") && username.includes(".")){
+      chrome.storage.sync.set(
+        {
+          username: username,
+          focusTabs: focusTabsCheckbox.checked,
+          searchCount: newSearchLimit,
+          customTimer: customTimer,
+          lastDate: today,
+          lastSearchCount: searchCount,
+        },
+        () => {
+          headerName.textContent = username;
+          updateUI(0, newSearchLimit);
+          console.log("Searched Query count: ", searchCount);
+          console.log("Search limit: ", newSearchLimit);
+          console.log("Custom timer: ", customTimer);
+          console.log("Last date: ", today);
+          console.log("Focus tabs: ", focusTabs.checked);
+          alert("Settings Saved!!! Please refresh the page.");
+        }
+      );
+    } else {
+      username = "Store the email id...";
+      return;
     }
-  );
+  } else {
+    chrome.storage.sync.set(
+      {
+        username: username,
+        focusTabs: focusTabsCheckbox.checked,
+        searchCount: newSearchLimit,
+        customTimer: customTimer,
+        lastDate: today,
+        lastSearchCount: searchCount,
+      },
+      () => {
+        updateUI(searchCount, newSearchLimit);
+        console.log("Searched Query count: ", searchCount);
+        console.log("Search limit: ", newSearchLimit);
+        console.log("Custom timer: ", customTimer);
+        console.log("Last date: ", today);
+        console.log("Focus tabs: ", focusTabs.checked);
+        alert("Settings Saved!!! Please refresh the page.");
+      }
+    );
+  }
 });
 // Reset all automation tasks
 restButton.addEventListener("click", () => {
@@ -114,6 +149,7 @@ restButton.addEventListener("click", () => {
   chrome.runtime.sendMessage({ action: "stopResetAutomation" });
   chrome.storage.sync.set(
     {
+      username: "Store the email id...",
       focusTabs: false,
       searchCount: 0,
       customTimer: 0,
